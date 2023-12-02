31eaf173f64ecca6e83f869ec0ed896d349226d3
GermanRaj <137941316+GermanRaj@users.noreply.github.com>
2023-12-02 19:31:13 +0200

    Homework_algo

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
-function findElem(arr1, arr2, k) {
-    const resultArr = [];
-    let i = 0;
-    let j = 0;
-  
-    while (i < arr1.length && j < arr2.length) {
-      if (arr1[i] < arr2[j]) {
-        resultArr.push(arr1[i]);
-        i++;
-      } else {
-        resultArr.push(arr2[j]);
-        j++;
-      }
+class Elevator {
+    constructor(currentFloor) {
+      this.currentFloor = currentFloor;
     }
   
-    while (i < arr1.length) {
-      resultArr.push(arr1[i]);
-      i++;
+    moveTo(floor) {
+      console.log(`Elevator moving from floor ${this.currentFloor} to floor ${floor}`);
+      this.currentFloor = floor;
     }
-  
-    while (j < arr2.length) {
-      resultArr.push(arr2[j]);
-      j++;
-    }
-  
-    return resultArr[k - 1];
   }
   
-  const arr1 = [100, 112, 256, 349, 770];
-  const arr2 = [72, 86, 113, 119, 265, 445, 892];
-  const k = 7;
+  class ElevatorSystem {
+    constructor(numFloors, numElevators) {
+      this.numFloors = numFloors;
+      this.elevators = Array.from({ length: numElevators }, (_, index) => new Elevator(index * 3)); // Инициализация лифтов
+    }
   
-  const result = findElem(arr1, arr2, k);
-  console.log(result);
+    callElevator(floor) {
+      const closestElevator = this.findClosestElevator(floor);
+      closestElevator.moveTo(floor);
+    }
+  
+    findClosestElevator(floor) {
+      let minDistance = Infinity;
+      let closestElevator = null;
+  
+      for (const elevator of this.elevators) {
+        const distance = Math.abs(elevator.currentFloor - floor);
+        if (distance < minDistance) {
+          minDistance = distance;
+          closestElevator = elevator;
+        }
+      }
+  
+      return closestElevator;
+    }
+  }
